f0e9fa3f443869866161bfbc5dd951f658eb2584
LazyPigZYH <152492591+Bersulang@users.noreply.github.com>
2024-11-10 20:10:14 +0800

    Add files via upload
    Some files about code

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,10 @@
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    a = 1012 * 2
+    return a
+
